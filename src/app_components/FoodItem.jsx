468c81fc508ee0976e4066d15097514a46b632da
techxtrasol <techxtrasol.design@gmail.com>
2025-07-06 21:55:41 +0300

    Refactor FoodItem component layout for improved structure and readability

--- src/app_components/FoodItem.jsx
+++ src/app_components/FoodItem.jsx
@@ -1,13 +1,18 @@
-const FoodItem = ({food}) => {
+const FoodItem = ({ food }) => {
   return (
     <>
       <div className="mx-auto justify-center">
-        <h3> {food.title} </h3>
-        <img src={food.image} alt={food.title} />
-        <button className="p-2 bg-green-600 text-white text-2xl rounded-md cursor-pointer my-3 font-bold">View Recipe</button>
+        <div className="container">
+          <h3> {food.title} </h3>
+          <img src={food.image} alt={food.title} />
+          <button
+            className="p-2 bg-green-600 text-white text-2xl rounded-md cursor-pointer my-3 font-bold">
+            View Recipe
+          </button>
+        </div>
       </div>
     </>
-   );
+  );
 }
 
 export default FoodItem;
